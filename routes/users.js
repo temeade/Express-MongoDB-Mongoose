50d34cafc68818963c9291ef0c8b423d6516e9df
Thomas Meade <te.meade@gmail.com>
2021-07-04 23:36:48 -0400

    Workshop 3

--- routes/users.js
+++ routes/users.js
@@ -6,7 +6,7 @@ const authenticate = require('../authenticate');
 const router = express.Router();
 
 /* GET users listing. */
-router.get('/', function (req, res, next) {
+router.get('/', authenticate.verifyAdmin, function (req, res, next) {
   res.send('respond with a resource');
 });
 
